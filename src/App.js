df72c5aeec576fa3cb8fb2c1d1f7d0953fd58062
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2022-04-10 19:39:58 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './components/Stars/Stars';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Stars count={'asd5646363636'} />
+      <Stars count={4} />
+      <Stars count={0} />
+    </div>
+  );
+}
+
+
+export default App;
+
